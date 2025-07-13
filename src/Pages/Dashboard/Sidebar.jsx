645862451fcd8ac71487c153c9214b646e219271
Ariful <arifulmit17@gmail.com>
2025-07-13 13:21:35 +0600

    added moderator dashboard

--- src/Pages/Dashboard/Sidebar.jsx
+++ src/Pages/Dashboard/Sidebar.jsx
@@ -28,6 +28,10 @@ const Sidebar = () => {
       {role=='admin' && <><li><Link to={'manageappliedapplication'}>Manage Applied Application </Link></li></>}
       {role=='admin' && <><li><Link to={'manageusers'}>Manage Users</Link></li></>}
       {role=='admin' && <><li><Link to={'managereview'}>Manage Review </Link></li></>}
+      {role=='moderator' && <><li><Link to={'addscholarship'}>Add Scholarship </Link></li></>}
+      {role=='moderator' && <><li><Link to={'managescholarship'}>Manage Scholarship </Link></li></>}
+      {role=='moderator' && <><li><Link to={'manageappliedapplication'}>Manage Applied Application  </Link></li></>}
+      {role=='moderator' && <><li><Link to={'managereview'}>Manage Review </Link></li></>}
     </ul>
   </div>
 </div>
