2d6474c8ae7e8dd8781dcd956264fbe978ecc8fc
Ariful <arifulmit17@gmail.com>
2025-07-13 13:21:17 +0600

    added moderator dashboard

--- src/Pages/Dashboard/Admin/UserRow.jsx
+++ src/Pages/Dashboard/Admin/UserRow.jsx
@@ -1,8 +1,36 @@
+import { useQueryClient, useMutation} from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
 
 const UserRow = ({user}) => {
+  const queryClient = useQueryClient()
+  const mutation = useMutation({
+    mutationFn: async role => {
+      const { data } = await axios.patch(
+        `${import.meta.env.VITE_API_URL}/user/role/update/${user.email}`,
+        { role }
+      )
+      return data
+    },
+    onSuccess: data => {
+      console.log(data)
+      // refetch()
+      // invalidate query
+      queryClient.invalidateQueries(['users'])
+    },
+    onError: error => {
+      console.log(error)
+    },
+  })
+    const handleChange=e=>{
+      
+      const changedRole=e.target.value;
+      console.log(changedRole);
+      mutation.mutate(changedRole)
+
+    }
     return (
-        <div>
+        
             <>
             <tr>
         <td>
@@ -19,6 +47,13 @@ const UserRow = ({user}) => {
         <td>{user.name}</td>
         <td>{user.email}</td>
         <td>{user.role}</td>
+
+        <td className='w-[150px]'><select onChange={handleChange} defaultValue={user?.role} className="select select-sm">
+          {/* <option disabled={true}>User</option> */}
+          <option value={"user"}>User</option>
+          <option value={"moderator"}>Moderator</option>
+            <option value={"admin"}>Admin</option>
+          </select></td>
         
         <th>
             <button  className="btn text-white bg-green-600 btn-xs">Delete</button>
@@ -28,7 +63,7 @@ const UserRow = ({user}) => {
         </th>
       </tr>
         </>
-        </div>
+        
     );
 };
 
