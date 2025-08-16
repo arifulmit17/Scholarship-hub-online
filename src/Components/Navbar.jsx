3332ab267b4a9065ac88fb6c073447ea795d128c
Ariful <arifulmit17@gmail.com>
2025-08-16 16:26:49 +0600

    fixed logo in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
         {links}
       </ul>
     </div>
-    <a className="btn btn-ghost ml-7 text-xl"> <span className='text-green-500 '>Scholarship-</span>Hub</a>
+    <a className="btn btn-ghost ml-7 text-xl"> <span className='text-green-500 '>Scholarship -</span>Hub</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
